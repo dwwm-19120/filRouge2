d27058f40a4280090546ab4090acdb585e736e1e
Augustin <56958607+Ashkoort@users.noreply.github.com>
2020-04-08 16:08:26 +0200

    je reprend ça demain matin ou aprem

---
+++ ci/application/controllers/fournisseurs.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Fournisseurs extends CI_Controller
+{
+    /**
+  * \brief 
+  * \return  
+  * \author LEGRIS Augustin
+  * \date 
+  */
+    public function fourniAjouts()
+    {
+        $this->templates->display('fourniAjouts');
+    } 
+
+}
