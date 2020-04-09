e3b53848bb5d7730bfebd644baebb02668b5c697
Augustin <56958607+Ashkoort@users.noreply.github.com>
2020-04-09 10:21:51 +0200

    petit changement

--- ci/application/views/footer.php
+++ ci/application/views/footer.php
@@ -1,12 +1,12 @@
 <footer>
         <p>Recevez <span>nos offres exclusives</span></p>
-        <?= form_open("" . ) ?>
+        <?= form_open("") ?>
         <input type="text" name="mailoffres" id="mailoffres" placeholder="Votre adresse e-mail">
         <input type="submit" name="valider" id="valider" value="Valider">
         <?= form_close(); ?>
 
         <p><span>Suivez-nous</span> par ici !</p>
-        <img id="visuel" src="<?php echo base_url('assets/img/FOOTER/') . ?>"></footer>
+        <img id="visuel" src="<?php echo base_url('assets/img/FOOTER/')?>"></footer>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
